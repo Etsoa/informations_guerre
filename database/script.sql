bf7b4e5febd4648e8d33910185dc7b5dd89216e4
Etsoa <fetraniaina26@gmail.com>
2026-03-30 13:56:21 +0300

    initialisation de la base de donnee

--- database/script.sql
+++ database/script.sql
@@ -1,20 +1,66 @@
--- ============================================
--- Script d'initialisation de la base de données
--- Ce fichier est exécuté automatiquement par
--- PostgreSQL au premier lancement du container
--- ============================================
+\c informations_guerre;
 
--- Ordre d'exécution dans docker-entrypoint-initdb.d :
---   1. script.sql  → Création des tables
---   2. data.sql    → Insertion des données
+-- Table utilisateurs
+CREATE TABLE utilisateurs(
+    id SERIAL PRIMARY KEY,
+    nom VARCHAR(100) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    mot_de_passe VARCHAR(255) NOT NULL
+);
 
--- Ajouter ici la création des tables
--- Exemple :
--- CREATE TABLE IF NOT EXISTS utilisateur (
---     id SERIAL PRIMARY KEY,
---     nom VARCHAR(100) NOT NULL,
---     email VARCHAR(255) UNIQUE NOT NULL,
---     mot_de_passe VARCHAR(255) NOT NULL,
---     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
---     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
--- );
+-- Table auteurs
+CREATE TABLE auteurs(
+    id SERIAL PRIMARY KEY,
+    nom VARCHAR(100) NOT NULL,
+    prenom VARCHAR(100) NOT NULL,
+    email VARCHAR(100)
+);
+
+-- Table catégories
+CREATE TABLE categories(
+    id SERIAL PRIMARY KEY,
+    nom VARCHAR(100) NOT NULL UNIQUE
+);
+
+-- Table articles
+CREATE TABLE articles(
+    id SERIAL PRIMARY KEY,
+    titre VARCHAR(250) NOT NULL,
+    description TEXT NOT NULL,
+    contenu TEXT NOT NULL,
+    date_publication TIMESTAMP
+);
+
+-- table sources
+CREATE TABLE sources(
+    id SERIAL PRIMARY KEY,
+    article_id INTEGER NOT NULL REFERENCES articles(id),
+    nom VARCHAR(100) NOT NULL UNIQUE,
+    url VARCHAR(255) NOT NULL
+);
+
+-- Table images 
+CREATE TABLE images(
+    id SERIAL PRIMARY KEY,
+    nom VARCHAR(255) NOT NULL,
+    article_id INTEGER NOT NULL REFERENCES articles(id)
+);
+
+-- Table article_auteur 
+CREATE TABLE article_auteur(
+    id SERIAL PRIMARY KEY,
+    article_id INTEGER NOT NULL REFERENCES articles(id),
+    auteur_id INTEGER NOT NULL REFERENCES auteurs(id)
+);
+
+-- Table article_categorie
+CREATE TABLE article_categorie(
+    id SERIAL PRIMARY KEY,
+    article_id INTEGER NOT NULL REFERENCES articles(id),
+    categorie_id INTEGER NOT NULL REFERENCES categories(id)
+);
+
+-- Index pour les performances
+CREATE INDEX idx_articles_date ON articles(date_publication);
+CREATE INDEX idx_images_article ON images(article_id);
+
